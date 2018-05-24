7d5dc176ef9d92099243487f6ba5df0647b10aaf
Minyoung Kim <carat_dvl12@naver.com>
2018-05-24 21:31:11 +0900

    project0524_1

---
+++ Assets/Script/MainScirpt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScirpt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(1920, 1280, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
