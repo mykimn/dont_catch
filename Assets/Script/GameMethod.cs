191604c9960744d7665dc8d5d81a597170b4bfe5
Minyoung Kim <carat_dvl12@naver.com>
2018-05-25 11:20:49 +0900

    project0525

---
+++ Assets/Script/GameMethod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMethod : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void GoGameMethod()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
